e5c78d9a98301a44de97902c7ff43736d2d081be
JAGx9 <jagonzalezf9@gmail.com>
2022-03-21 20:07:06 -0700

    sending index and show data

---
+++ server.js
@@ -0,0 +1,22 @@
+//Express Server
+const express = require ('express')
+
+//Configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+//Routes
+app.get('/', (req,res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+
+//Breads
+const breadsController = require ('./controllers/bread_controller.js')
+app.use('/breads', breadsController)
+
+//Listen
+app.listen(PORT, () => {
+    console.log('Listening at port', PORT)
+})
